fab601a7ed6ae271166e57144d0f3a8e65dee3f4
Dylan Luu <dylanluu@ucsb.edu>
2023-01-17 11:30:51 -0500

    Testing skeleton

--- react-app/src/components/VideoCards/index.js
+++ react-app/src/components/VideoCards/index.js
@@ -1,14 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './VideoCards.css';
 import { useDispatch, useSelector } from 'react-redux';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function VideoCards({video}) {
 
     const users = useSelector(state => state.session.allUsers);
     const videoUser = users?.find(user => user.id == video.user_id);
+    const [isLoaded, setIsLoaded] = useState(false)
+    setTimeout(() => setIsLoaded(true), 225)
 
     if(!videoUser) return null;
+
+    if(!isLoaded) return (
+        <div className='video-cards-container'>
+            <span className='video-card-thumbnail'>
+                <Skeleton
+                className='video-card-thumbnail-image'
+                borderRadius='12px'
+                height='153px'
+                width='258px'
+                baseColor='#252525'
+                highlightColor='#252525'
+                />
+            </span>
+            <div className='video-card-info'>
+                <div className='video-card-profile-pic' style={{backgroundColor: 'transparent'}}>
+                    <Skeleton
+                    height='40px'
+                    width='40px'
+                    baseColor='#252525'
+                    highlightColor='#252525'
+                    borderRadius='50%'
+                    />
+                </div>
+                <div className='video-card-title-and-info'>
+                    <Skeleton
+                    height='15px'
+                    width='150px'
+                    baseColor='#252525'
+                    highlightColor='#252525'
+                    />
+                <Skeleton
+                height='15px'
+                width='100px'
+                baseColor='#252525'
+                highlightColor='#252525'
+                className='margin-top'
+                />
+                </div>
+            </div>
+        </div>
+    )
 
     return (
         <div className='video-cards-container'>
